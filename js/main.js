b75dff5932c2c1ccb0e795abe684130d1a50dbdd
Alvaro Concha <aconchauriol@gmail.com>
2016-02-15 15:09:39 -0500

    efecto seleccion para mobile

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
       event.preventDefault();
       if ($(window).width() < 600) {
         $('html, body').animate({
-              scrollTop: $(target).offset().top - 315
+              scrollTop: $(target).offset().top - 60
           }, 1000);
       }
       else {
